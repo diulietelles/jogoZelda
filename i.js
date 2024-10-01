cf1bc563fc23fd91b6eb5e0d3c688ced0f5e5e54
diulietelles <tellesdiulie@gmail.com>
2024-10-01 16:53:38 -0300

    adicionar arquivos

--- i.js
+++ i.js
@@ -39,7 +39,24 @@ let somaPares = 0
 
 for(let i = 0; i <numeros.length; i++){
     if(numeros[i] % 2 === 0)
-        console.log(`i: ${i},numeros[i]: ${numeros[i]}, soma antes: ${somaPares}`)
+        // console.log(`i: ${i},numeros[i]: ${numeros[i]},`)
     somaPares += numeros[i]
 }
 console.log(`resultado final ${somaPares}`)
+
+
+function jogoPontuação(jogadas){
+    let pontuacao = 0
+
+    for(let i = 0; i <=jogadas.length; i++){
+        if(jogadas[i]=== 'ganhou'){
+            pontuacao +=3
+        } else if(jogadas[i]=== 'empatou'){
+
+        } else if(jogadas[i] === 'perdeu'){
+            pontuacao -=1
+        }
+    }
+    return pontuacao
+}let resultadoFinal = jogoPontuação(['ganhou', 'empatou', 'perdeu','ganhou'])
+console.log(`Pontuacao final ${resultadoFinal}`) // corrija os erros e veja o resultado
